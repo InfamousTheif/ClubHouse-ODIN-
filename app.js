95194cc3f4070d18c64b2aea318ee6ae11d55499
inquntum <inquntum@gmail.com>
2026-03-16 19:05:18 +0800

    built and styled index page

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ import { indexRouter } from "./routes/indexRoute.js";
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
 app.use(indexRouter);
 
