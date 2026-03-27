44230ccae521c7638ba8727ed29b3f0caede1ac6
inquntum <inquntum@gmail.com>
2026-03-27 16:27:02 +0800

    imported session and connect pgSimple and configured them

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ import express from "express";
 const app = express();
 import './utils/dotenv.js';
 import { indexRouter } from "./routes/indexRoute.js";
+import session from "express-session";
+import connectPgSimple from "connect-pg-simple";
+const pgSession = connectPgSimple(session);
+import { pool } from "./db/pool.js";
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: new pgSession({
+    pool: pool,
+    tableName: 'userInfo'
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24
+  }
+}))
 
 app.use(indexRouter);
 
