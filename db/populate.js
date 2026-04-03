5b3c005b32d1c04a0733c94fa87cf319793c0ae5
inquntum <inquntum@gmail.com>
2026-04-03 19:02:51 +0800

    changed the max lengths of the usernames in each table '10 --> 20'

--- db/populate.js
+++ db/populate.js
@@ -4,7 +4,7 @@ import '../utils/dotenv.js';
 const SQL = `
   CREATE TABLE IF NOT EXISTS userInfo (
     userId INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    username VARCHAR(10) NOT NULL CHECK(LENGTH(TRIM(username)) > 0),
+    username VARCHAR(20) NOT NULL CHECK(LENGTH(TRIM(username)) > 0),
     password VARCHAR(255) NOT NULL CHECK(LENGTH(TRIM(password)) > 0),
     email VARCHAR(255) NOT NULL CHECK(LENGTH(TRIM(password)) > 0),
     memberStatus BOOLEAN NOT NULL DEFAULT FALSE,
@@ -15,7 +15,7 @@ const SQL = `
   CREATE TABLE IF NOT EXISTS userPost (
     postId INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     userId INTEGER,
-    username VARCHAR(10) NOT NULL CHECK(LENGTH(TRIM(username)) > 0),
+    username VARCHAR(20) NOT NULL CHECK(LENGTH(TRIM(username)) > 0),
     title VARCHAR(50) NOT NULL CHECK(LENGTH(TRIM(username)) > 0),
     post VARCHAR(255) NOT NULL CHECK(LENGTH(TRIM(post)) > 0),
     sendDate TIMESTAMPTZ DEFAULT now()
