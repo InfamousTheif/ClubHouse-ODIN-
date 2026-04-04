47a81f840e07f26b4d1af3138c66ae30b58031b6
inquntum <inquntum@gmail.com>
2026-04-04 09:42:47 +0800

    imported validationResult and matchedData from express-validator and used them to validate the query from the log-in path. The general err page is rendered, with a custom err msg, if a error occurs

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 import * as db from "../db/queries.js";
 import * as dateHandler from "../utils/dateFormatter.js";
+import { validationResult, matchedData } from "express-validator";
 
 async function renderIndexPage(req, res) {
   const title = 'Mickey\'s Clubhouse';
@@ -16,7 +17,15 @@ function renderSignUp(req, res) {
 
 function renderLogIn(req, res) {
   const title = "Log In";
-  const { status } = req.query;
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    const errCode = 500;
+    const errMsg = errors.array()[0].msg;
+    return res.render("displayErr", { title, errCode, errMsg });
+  }
+
+  const data = matchedData(req);
+  const { status } = data
 
   res.render('log-in', { title, status });
 };
