bd078ae55c6ef6273c2aed90881de18eb658a614
inquntum <inquntum@gmail.com>
2026-04-04 11:29:56 +0800

    deleted redundant methods and added the post validator to the post route

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -19,11 +19,11 @@ indexRouter.get("/Log-out", indexController.handleLogOut);
 
 indexRouter.post("/Sign-up", postControllers.signUpValidator, postControllers.handleSignIn);
 
-indexRouter.post("/Log-in", passport.authenticate('local', {failureRedirect:"/Log-in?status=failed", successRedirect:"/" }), postControllers.handleLogIn);
+indexRouter.post("/Log-in", passport.authenticate('local', {failureRedirect:"/Log-in?status=failed", successRedirect:"/" }));
 
 indexRouter.post("/member-initiation", postControllers.handleMemberInitiation);
 
-indexRouter.post("/", postControllers.handleuserPosts);
+indexRouter.post("/", postControllers.postValidator, postControllers.handleuserPosts);
 
 indexRouter.post("/delete-post", postControllers.handleDeletePost);
 
