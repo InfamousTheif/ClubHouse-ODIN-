77ca370f610d4126b88cb678d3800316e71367d7
inquntum <inquntum@gmail.com>
2026-04-04 11:31:10 +0800

    styled new classes and used the new 'flex' class to hide/ show the form by adding it/ removing it using the post-form.js file

--- public/post-form.js
+++ public/post-form.js
@@ -3,9 +3,9 @@ const postForm = document.querySelector(".post-form-popup");
 const showPostForm = document.querySelector(".button-wrapper__post-button")
 
 closeFormButton.addEventListener('click', () => {
-  postForm.style.display = "none";
+  postForm.classList.remove("flex");
 });
 
 showPostForm.addEventListener('click', () => {
-  postForm.style.display = "flex";
+  postForm.classList.add("flex");
 });
